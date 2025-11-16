302375615f3717373e43414707020ecfc25dd0be
ganzihai <ganzihai@qq.com>
2025-11-16 18:59:48 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -123,7 +123,7 @@ const API_SITES = {
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
-        api: 'https://www.ckzy1.com/api.php/provide/vod',
+        api: 'https://ckzy.me/api.php/provide/vod',
         name: 'CK资源',
         adult: true
     },
@@ -133,8 +133,8 @@ const API_SITES = {
         adult: true
     },
     bwzy: {
-        api: 'https://api.bwzym3u8.com/api.php/provide/vod',
-        name: '百万资源',
+        api: 'https://api.douapi.cc/api.php/provide/vod',
+        name: '豆豆资源',
         adult: true
     },
     souav: {
